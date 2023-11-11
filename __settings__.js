66c20cce5f95417c0b4d7fab3f2d1744a9750e07
Denys Smirnov <dendi239@gmail.com>
2023-11-11 18:49:41 +0200

    update code

--- __settings__.js
+++ __settings__.js
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'preferWebGl2': true,
     'powerPreference': "high-performance"
 };
-window.SCRIPTS = [ 156899320 ];
+window.SCRIPTS = [ 156899320, 156899551, 156903883 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
