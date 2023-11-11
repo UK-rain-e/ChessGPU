ae5c2180a8e6b628861efdde91e89ccfbcffb211
Denys Smirnov <dendi239@gmail.com>
2023-11-11 16:41:55 +0200

    add basic base version

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1897802.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 156899320 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/156899326/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/156899327/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/156899325/1/ammo.js', 'preload' : true},
+];
---
+++ rapid-rook.js
@@ -0,0 +1,19 @@
+// const helloWorld = require(".");
+
+window.addEventListener("DOMContentLoaded", function () {
+    setInterval(
+        () => {
+            console.log("check");
+            if (typeof pc !== 'undefined') {
+                mvRook();
+            }
+        }, 2000
+    );
+});
+
+function mvRook() {
+    var rook = pc.app.root.findByName("ROOK");
+    var x = Math.ceil(Math.random() * 7 - 3) - 0.5;
+    var y = Math.ceil(Math.random() * 7 - 3) - 0.5;
+    rook.rigidbody.teleport(x, 1, y);
+}
