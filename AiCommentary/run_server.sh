2dbfd62f11cd6a9db0db54051d840f4b02a045ac
Michael Babenko <mishababenko2@gmail.com>
2023-11-12 07:08:29 +0200

    fix run_server

--- AiCommentary/run_server.sh
+++ AiCommentary/run_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source .venv/bin/activate
-uvicorn server:app --host 0.0.0.0 --port 5539 --ssl-keyfile=./key.pem --ssl-certfile=./cert.pem
+uvicorn server:app --host 0.0.0.0 --port 5539
